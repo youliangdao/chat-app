f999243557eb7077888981d3550a182ba01c9cc4
youliangdao <youliangdaojing5@gmail.com>
2020-10-12 22:33:04 +0900

    プルダウンの実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,17 @@
 class RoomsController < ApplicationController
   def new
     @room = Room.new
+    @users = User.where.not(id: current_user.id)
+  end
+  def create
+    Room.create(name: room_params, user_ids: [current_user.id, user_params)
+  end
+
+  private
+  def room_params
+    params.require(room).permit(:name)
+  end
+  def user_params
+    params.require(room).permit(:user_id)
   end
 end
