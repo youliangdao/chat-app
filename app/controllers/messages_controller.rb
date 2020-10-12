b5704a71f1f6813fd5c1bcaf4f20940f3f9cf1f0
youliangdao <youliangdaojing5@gmail.com>
2020-10-13 05:46:55 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,16 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @messages = @room.messages
-    @message = @messages.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
